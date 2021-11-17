0939754f96dc646945dfac7082e1815b0efcaaf9
Makar <jsaad41@gmail.com>
2021-11-17 23:57:15 +1100

    jump and coin grab

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -59,27 +59,19 @@ public class Movement : MonoBehaviour
 
     private void collectInput()
     {
-       // moveHorizontal = Input.GetAxis("Horizontal");
-        //moveVertical = Input.GetAxis("Vertical");
-        RightController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 axisValue);
+
+        LeftController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 axisValue);
         moveHorizontal = axisValue.x;
         moveVertical = axisValue.y;
-        Transform[] ts = target.GetComponentsInChildren<Transform>();
-
-        if (ts.Length > 1)
-        {
-
-           /* moveX = Input.GetAxis("Mouse X");
-            moveY = Input.GetAxis("Mouse Y");*/
-        }
-        LeftController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 lookAxisValue);
+        
+        RightController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 lookAxisValue);
         moveX = lookAxisValue.x;
         moveY = lookAxisValue.y;
     }
 
     void jetpackUse()
     {
-        if (Input.GetKeyDown(KeyCode.E) && canUseJetPack)
+        if (RightController.TryGetFeatureValue(CommonUsages.secondaryButton, out bool pressed) && canUseJetPack)
         {
             rb.AddForce(Vector3.up * 45, ForceMode.Impulse);
             canUseJetPack = false;
@@ -106,8 +98,7 @@ public class Movement : MonoBehaviour
     private void jump()
     {
         RightController.TryGetFeatureValue(CommonUsages.primaryButton, out bool buttonPressed);
-        Debug.Log(isGrounded);
-        if ((Input.GetKeyDown(KeyCode.Space) || buttonPressed) && isGrounded)
+        if (buttonPressed && isGrounded)
         {
             rb.AddForce(new Vector3(0, 2.0f, 0) * jumpForce, ForceMode.Impulse);
         }
